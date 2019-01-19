237e1a02e6800c0026a265dd76ef753eb8f0bf83
ericmagallan <ericmagallan85@gmail.com>
2019-01-19 10:56:15 -0800

    Pushing up week 11 assignments up to day 2.

---
+++ week_11/day3/Stu_FindShow.js
@@ -0,0 +1,10 @@
+var TV = function() {
+    this.findShow = function(show) {
+      // The following URL can be used to search the TV Maze API for a given show
+      var URL = "http://api.tvmaze.com/singlesearch/shows?q=" + show;
+  
+    };
+  };
+  
+  module.exports = TV;
+  
---
+++ week_11/day3/cli.js
@@ -0,0 +1,9 @@
+var searchType = process.argv [2];
+var searchTerm = process.argv.slice(3).join(" ");.toLowerCase();
+
+if(searchType !=== "actor" || searchType === "show"){
+searchType = "show";
+
+if(searchTerm !=== "actor" || searchTerm !=== "show") {}
+}
+console.log(searchTerm);
